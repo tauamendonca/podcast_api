116090abd55e8bae35b5f812b531facbc7217fd8
Tauã Mendonça <63937691+tauamendonca@users.noreply.github.com>
2025-09-07 21:18:24 -0300

    added http and scripts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("VAI SE FUDE")
+import * as http from "http";
+
+const server = http.createServer((
+    request: http.IncomingMessage, 
+    response: http.ServerResponse
+) => {});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado na porta ${process.env.PORT}`)
+});
